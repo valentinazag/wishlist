6da4e6fc07fef782d953945a19e534d5ee5fe15b
valentinazag <valentinazagman@gmail.com>
2026-01-19 18:15:46 +0000

    products endpoint

---
+++ src/interface/product.interface.ts
@@ -0,0 +1,11 @@
+export interface Product{
+id: string;
+title: string;
+category: string;
+price: number;
+currency: string;
+stock: number;
+rating: number;
+imageUrl: string;
+createdAt: string;
+}
